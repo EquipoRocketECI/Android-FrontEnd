4c849be0fc8ce9a2156e52b0dc9f76c1510fa24a
Daniel Cifuentes <danixbox1313@gmail.com>
2020-11-18 19:13:29 -0500

    added filtering to ideaServices

--- app/src/main/java/equipo/rocket/headhunters/services/IdeaServices.java
+++ app/src/main/java/equipo/rocket/headhunters/services/IdeaServices.java
@@ -1,11 +1,22 @@
 package equipo.rocket.headhunters.services;
 
 
+
+
+
+
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
 import java.util.List;
 
 import equipo.rocket.headhunters.model.Idea;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 public interface IdeaServices {
 
@@ -13,4 +24,7 @@ public interface IdeaServices {
 
     @GET("/ideas")
     Call<List<Idea>> getAllIdeas();
+
+    @POST("/ideas/filtered")
+    Call<List<Idea>> filter(@Body JsonObject selectedFilters);
 }
--- app/src/main/java/equipo/rocket/headhunters/ui/explore/ExploreFragment.java
+++ app/src/main/java/equipo/rocket/headhunters/ui/explore/ExploreFragment.java
@@ -11,20 +11,14 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import equipo.rocket.headhunters.R;
 import equipo.rocket.headhunters.model.Idea;
@@ -44,6 +38,7 @@ public class ExploreFragment extends Fragment {
     protected IdeaCardAdapter mAdapter;
 
     protected List<Idea> mIdeasList;
+    protected JsonObject mSelectedFilters;
 
     @Nullable
     @Override
@@ -63,6 +58,7 @@ public class ExploreFragment extends Fragment {
         mRecyclerView.setAdapter(mAdapter);
 
         initIdeasList();
+        //filter();
 
         return rootView;
     }
@@ -76,6 +72,7 @@ public class ExploreFragment extends Fragment {
         IdeaServices ideaServices = retrofit.create(IdeaServices.class);
 
         Call<List<Idea>> getAllIdeas = ideaServices.getAllIdeas();
+        Log.d(this.getClass().getSimpleName(),getAllIdeas.request().toString());
         getAllIdeas.enqueue(new Callback<List<Idea>>() {
             @Override
             public void onResponse(Call<List<Idea>> call, Response<List<Idea>> response) {
@@ -88,6 +85,31 @@ public class ExploreFragment extends Fragment {
             public void onFailure(Call<List<Idea>> call, Throwable t) {
 
                 Log.d(this.getClass().getSimpleName(),t.getMessage() +"|||||||||||||||||||");
+            }
+        });
+    }
+
+    private void filter(){
+        final Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://mysterious-refuge-36454.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        IdeaServices ideaServices = retrofit.create(IdeaServices.class);
+        mSelectedFilters = new JsonParser().parse("{ \"selectedCategories\":{ \"Moda\":true}}").getAsJsonObject();//change to gson, avoid valuepair
+
+        Call<List<Idea>> filter = ideaServices.filter(mSelectedFilters);
+        filter.enqueue(new Callback<List<Idea>>() {
+            @Override
+            public void onResponse(Call<List<Idea>> call, Response<List<Idea>> response) {
+                mIdeasList=response.body();
+                mAdapter.setmIdeasList(mIdeasList);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Idea>> call, Throwable t) {
+                Log.d(this.getClass().getSimpleName(),t.getMessage()+"||||||----");
             }
         });
     }
