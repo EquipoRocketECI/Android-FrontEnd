3bb026116008c21d761dffdb5c98782d28226692
Mateo Gonzalez <matteu20@hotmail.com>
2020-11-26 14:43:08 -0500

    arreglos al get del login y post signin

--- app/src/main/java/equipo/rocket/headhunters/ui/Login/LoginFragment.java
+++ app/src/main/java/equipo/rocket/headhunters/ui/Login/LoginFragment.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 import equipo.rocket.headhunters.R;
 import equipo.rocket.headhunters.model.Login;
 import equipo.rocket.headhunters.services.LoginServices;
+import equipo.rocket.headhunters.ui.destacadas.HomeFragment;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -32,57 +38,21 @@ public class LoginFragment extends Fragment {
     private LoginViewModel loginViewModel;
 
 
+    public static String getSHA256(String input){
 
-    private void LoginClicked(View root){
+        String toReturn = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.reset();
+            digest.update(input.getBytes("utf8"));
+            toReturn = String.format("%064x", new BigInteger(1, digest.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        final Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(LoginServices.LOGIN_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        return toReturn;
+    }
 
-        LoginServices loginServices = retrofit.create(LoginServices.class);
-        final EditText correo = root.findViewById(R.id.text_correo);
-        final EditText contrasena = root.findViewById(R.id.text_contrasena);
-
-        correo.getText();
-        contrasena.getText();
-
-        retrofit2.Call<Login> UsuarioByCorreo = loginServices.getUsuario(correo.getText().toString());
-        UsuarioByCorreo.enqueue(new Callback<Login>() {
-
-            @Override
-            public void onResponse(retrofit2.Call<Login> call, Response<Login> response) {
-                mLogin = response.body();
-                if (mLogin.getContrasena()==contrasena.getText().toString()){
-
-                }
-                else{
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                    builder.setTitle("Error");
-                    builder.setMessage("La contrasena es incorrecta");
-                    builder.setPositiveButton("Aceptar", null);
-
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(retrofit2.Call<Login> call, Throwable t) {
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("Error");
-                builder.setMessage("El correo no existe");
-                builder.setPositiveButton("Aceptar", null);
-
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-
-
-        });
-    };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -90,7 +60,76 @@ public class LoginFragment extends Fragment {
     loginViewModel = new ViewModelProvider(this).get(LoginViewModel .class);
     View root = inflater.inflate(R.layout.fragment_login, container, false);
 
+        Button Ingresar= root.findViewById(R.id.accionLoguearse);;
 
+
+
+        Ingresar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.println("lalal");
+                final Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(LoginServices.LOGIN_BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                LoginServices loginServices = retrofit.create(LoginServices.class);
+                final EditText correo = root.findViewById(R.id.text_correo);
+                final EditText contrasena = root.findViewById(R.id.text_contrasena);
+
+                correo.getText();
+                contrasena.getText();
+
+
+                String nuevaContrasena = getSHA256(contrasena.getText().toString());
+
+                retrofit2.Call<Login> UsuarioByCorreo = loginServices.getUsuario(correo.getText().toString());
+                UsuarioByCorreo.enqueue(new Callback<Login>() {
+
+                    @Override
+                    public void onResponse(retrofit2.Call<Login> call, Response<Login> response) {
+                        mLogin = response.body();
+                        System.out.println(mLogin.getContrasena());
+                        System.out.println(nuevaContrasena);
+                        if (mLogin.getContrasena().equals(nuevaContrasena)){
+                            Fragment nuevoFragmento = new HomeFragment();
+                            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+                            transaction.replace(R.id.nav_login,nuevoFragmento );
+                            transaction.addToBackStack(null);
+
+                            // Commit a la transacción
+                            transaction.commit();
+
+                        }
+                        else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                            builder.setTitle("Error");
+                            builder.setMessage("La contrasena es incorrecta");
+                            builder.setPositiveButton("Aceptar", null);
+
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<Login> call, Throwable t) {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle("Error");
+                        builder.setMessage("El correo no existe");
+                        builder.setPositiveButton("Aceptar", null);
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+
+
+                });
+            }
+        });
     //set correo
     final TextView correo = root.findViewById(R.id.text_correo);
         loginViewModel.getCorreo().observe(getViewLifecycleOwner(), new Observer<String>() {
--- app/src/main/java/equipo/rocket/headhunters/ui/SignIn/SignInFragment.java
+++ app/src/main/java/equipo/rocket/headhunters/ui/SignIn/SignInFragment.java
@@ -1,21 +1,51 @@
 package equipo.rocket.headhunters.ui.SignIn;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 import equipo.rocket.headhunters.R;
+import equipo.rocket.headhunters.model.Login;
+import equipo.rocket.headhunters.services.LoginServices;
+import equipo.rocket.headhunters.ui.Login.LoginFragment;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class SignInFragment extends Fragment {
+
+    protected Login mSignIn;
+    public static String getSHA256(String input){
+
+        String toReturn = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.reset();
+            digest.update(input.getBytes("utf8"));
+            toReturn = String.format("%064x", new BigInteger(1, digest.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return toReturn;
+    }
 
     private SignInViewModel signinViewModel;
 
@@ -25,15 +55,86 @@ public class SignInFragment extends Fragment {
     signinViewModel = new ViewModelProvider(this).get(SignInViewModel .class);
     View root = inflater.inflate(R.layout.fragment_signin, container, false);
 
+        Button Registrarse= root.findViewById(R.id.accionRegistarse);;
+
+        Login signin = new Login();
+
+
+
+        Registrarse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final EditText correo = root.findViewById(R.id.text_nuevocorreo);
+                final EditText contrasena = root.findViewById(R.id.text_nuevacontrasena);
+                final EditText nombres = root.findViewById(R.id.text_nombres);
+                final EditText apellidos = root.findViewById(R.id.text_apellidos);
+                String nuevoPostContrasena = getSHA256(contrasena.getText().toString());
+
+                correo.getText();
+                contrasena.getText();
+                nombres.getText();
+                apellidos.getText();
+
+                signin.setCorreo(correo.getText().toString());
+                signin.setContrasena(nuevoPostContrasena);
+                signin.setNombrecompleto(nombres.getText().toString()+" "+apellidos.getText().toString());
+
+                final Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(LoginServices.LOGIN_BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                LoginServices loginServices = retrofit.create(LoginServices.class);
+
+                retrofit2.Call<Login> PostUsuario = loginServices.addUser(signin);
+                PostUsuario.enqueue(new Callback<Login>() {
+
+                    @Override
+                    public void onResponse(retrofit2.Call<Login> call, Response<Login> response) {
+
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                            builder.setTitle("Correcto");
+                            builder.setMessage("El usuario se creo correctamente");
+                            builder.setPositiveButton("Aceptar", null);
+
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+
+                            Fragment nuevoFragmento = new LoginFragment();
+                            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+                            transaction.replace(R.id.nav_signin,nuevoFragmento );
+                            transaction.addToBackStack(null);
+                            // Commit a la transacción
+                            transaction.commit();
+
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<Login> call, Throwable t) {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle("Error");
+                        builder.setMessage("Nombre de usuario ya tomado");
+                        builder.setPositiveButton("Aceptar", null);
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+
+
+                });
+            }
+        });
     //set correo
-    final TextView correo = root.findViewById(R.id.text_correo);
+    final TextView correo = root.findViewById(R.id.text_nuevocorreo);
         signinViewModel.getCorreo().observe(getViewLifecycleOwner(), new Observer<String>() {
         @Override
         public void onChanged(@Nullable String s) { correo.setText(s); }
     });
 
     //set contrasena
-    final TextView contrasena = root.findViewById(R.id.text_contrasena);
+    final TextView contrasena = root.findViewById(R.id.text_nuevacontrasena);
         signinViewModel.getContrasena().observe(getViewLifecycleOwner(), new Observer<String>() {
         @Override
         public void onChanged(@Nullable String s) { contrasena.setText(s); }
